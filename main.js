f4d79e1ac088c787d49d2ad9ca4143cda72b847c
KANASHI2 <godaabdallah71@gmail.com>
2023-07-08 07:54:36 +0300

    heelloooo commit

---
+++ main.js
@@ -0,0 +1,40 @@
+let text = document.querySelector(".second-text")
+
+let textLoad = () =>{
+  setTimeout(() => {
+    text.textContent = "16 Years Old"
+  }, 0);
+  setTimeout(() => {
+    text.textContent = "Freelancer"
+  }, 4000);
+  setTimeout(() => {
+    text.textContent = "Front End Developer"
+  }, 8000);
+  setTimeout(() => {
+    text.textContent = "Fu**ing Your A**"
+  }, 12000);
+}
+textLoad();
+setInterval(textLoad, 16000);
+
+let menu = document.querySelector(".nav-icon")
+let nav = document.querySelector("nav")
+
+menu.addEventListener('click', function(){
+  nav.classList.toggle('active-nav')
+})
+
+let btn = document.querySelector('.top-btn')
+console.log(btn)
+
+window.onscroll = function(){
+  if(scrollY >= 600){
+    btn.style.display = 'block'
+  }else{btn.style.display = 'none'}
+}
+btn.onclick = function(){
+  scroll({
+    top: 0,
+    behavior: "smooth"
+  })
+}
